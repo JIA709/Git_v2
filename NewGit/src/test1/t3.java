1765b8564816844d39d1e964d963a828dc802db4
RickChou <chou830905@gmail.com>
2023-01-07 16:04:41 +0800

    test utf

--- NewGit/src/test1/t3.java
+++ NewGit/src/test1/t3.java
@@ -2,6 +2,6 @@ package test1;
 
 public class t3 {
 	public static void main(String[] args) {
-		System.out.println("盟鎮帥喔");
+		System.out.println();
 	}
 }
--- NewGit/src/test1/t5.java
+++ NewGit/src/test1/t5.java
@@ -2,6 +2,6 @@ package test1;
 
 public class t5 {
 	public static void main(String[] args) {
-		System.out.println("�Ÿ۫ӳ�");
+		System.out.println("測試");
 	}
 }
